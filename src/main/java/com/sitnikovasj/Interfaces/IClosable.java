012516a2756e8db528d59f76f1b4b04d0346894a
SonyaSitnikova <sitnikovasj@gmail.com>
2017-11-23 17:37:55 +0600

    HomeWork #4

--- src/main/java/com/sitnikovasj/Interfaces/IClosable.java
+++
@@ -1,9 +0,0 @@
-package com.sitnikovasj.Interfaces;
-
-import com.sitnikovasj.Exceptions.CloseException;
-import com.sitnikovasj.Exceptions.WriterException;
-
-public interface IClosable extends AutoCloseable{
-    @Override
-    void close() throws CloseException;
-}
